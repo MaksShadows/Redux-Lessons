b1f3d2000c7b853e734d93bb9d061d4e57bc106f
MaksShadows <Lykanenko1995@gmail.com>
2020-06-01 14:44:58 +0300

    task2

---
+++ Lesson1/task2/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import { UserContext } from "./user-context";
+
+class App extends Component {
+    state = {
+        userData: {
+            name: "Nikola Tesla",
+            avatar_url: "https://avatars3.githubusercontent.com/u10001",
+        },
+    };
+
+    render() {
+        return (
+            <div className="page">
+            <userContext.Provider value={this.state.userData}>
+              <Header />
+            </userContext.Provider>
+          </div>
+        );
+    }
+}
+
+export default App;
