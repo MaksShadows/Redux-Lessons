accf1aafb79d235cad069fbf01136dd05f9f45bc
MaksShadows <Lykanenko1995@gmail.com>
2020-06-02 13:16:19 +0300

    task2

--- Lesson3/task2/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from './App.jsx';
-import './index.scss';
-
-const rootElement = document.querySelector('#root');
-
-
-ReactDOM.render(<App />, rootElement);
