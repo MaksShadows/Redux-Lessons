7916a8a51b9f83365b9545b5a8b89e0f7ecb327f
MaksShadows <Lykanenko1995@gmail.com>
2020-06-04 15:15:24 +0300

    3

--- Lesson6/task3/src/weather/weather.actions.js
+++ Lesson6/task3/src/weather/weather.actions.js
@@ -13,9 +13,16 @@ export const weatherDataReceived = (weatherData) => {
 };
 
 
+const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/cities";
+
 export const getWeatherData = () => {
     return function (dispatch) {
-        fetchWeatherData()
-            .then(weatherData => dispatch(weatherDataReceived(weatherData)))
-    }
-}
+        fetch(baseUrl)
+            .then((response) => {
+                if (response.ok) return response.json();
+
+                throw new Error("Failed to load");
+            })
+            .then((weatherData) => dispatch(weatherDataReceived(weatherData)));
+    };
+};
